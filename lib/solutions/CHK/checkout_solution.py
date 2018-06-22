7f695d641bd98c804e457ff41a3896944f7da74c
sbx_user1060 <sbx_user1060@ip-10-12-4-196.eu-west-2.compute.internal>
2018-06-22 18:03:48 +0000

    Fri Jun 22 18:03:48 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -28,30 +28,6 @@ def checkout(skus):
         n2 = result["B"] % 2
         output = 45*n1 + n2*30
 
-
-
-
-
-    # if skus == "A":
-    #
-    #     output = 50
-    # elif skus == "B":
-    #     output = 30
-    #
-    # elif skus == "C":
-    #     output = 20*n
-    # elif skus == "D":
-    #     output = 15*n
-    # elif skus == "":
-    #     output = 0
-    # elif skus == "AAA":
-    #     output = 130
-    # elif skus == "ABCD":
-    #     output = 115
-    # elif skus == "AA":
-    #     output = 100
-    # elif skus == "BB":
-    #     output = 45
     else:
         return -1
 
