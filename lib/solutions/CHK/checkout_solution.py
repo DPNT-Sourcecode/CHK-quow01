08be17952a22f4fffd6da4d455b63b62395376de
sbx_user1060 <sbx_user1060@ip-10-12-4-196.eu-west-2.compute.internal>
2018-06-22 18:13:26 +0000

    Fri Jun 22 18:13:26 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -6,34 +6,55 @@ def checkout(skus):
 
     output = 0
     n = 0
-    result = {}
-    for i in skus[0]:
-        n +=1
-
-        result[i] = n
-
-    print(result)
-
-    if result["A"] >= 3:
-        n1 = result["A"] / 3
-        n2 = result["A"] % 3
-
-        output = 130*n1 + n2*50
-
-    elif result["A"] < 3:
-        output = result["A"]*50
-
-    elif result["B"] >= 2:
-        n1 = result["B"] / 2
-        n2 = result["B"] % 2
-        output = 45*n1 + n2*30
-    elif result["C"]:
-        output = 20*result["C"]
-    elif result["C"]:
-        output = 15 * result["D"]
+    # result = {}
+    # for i in skus[0]:
+    #     n +=1
+    #
+    #     result[i] = n
+    #
+    # print(result)
+    #
+    #
+    # if result["A"] >= 3:
+    #     n1 = result["A"] / 3
+    #     n2 = result["A"] % 3
+    #
+    #     output = 130*n1 + n2*50
+    #
+    # elif result["A"] < 3:
+    #     output = result["A"]*50
+    #
+    # elif result["B"] >= 2:
+    #     n1 = result["B"] / 2
+    #     n2 = result["B"] % 2
+    #     output = 45*n1 + n2*30
+    # elif skus == "C":
+    #     output = 20*result["C"]
+    # elif skus == "D":
+    #     output = 15 * result["D"]
     # elif skus == "":
     #     output = 0
 
+    if skus == "A":
+
+        output = 50
+    elif skus == "B":
+        output = 30
+
+    elif skus == "C":
+        output = 20*n
+    elif skus == "D":
+        output = 15*n
+    elif skus == "":
+        output = 0
+    elif skus == "AAA":
+        output = 130
+    elif skus == "ABCD":
+        output = 115
+    elif skus == "AA":
+        output = 100
+    elif skus == "BB":
+        output = 45
     else:
         return -1
 
