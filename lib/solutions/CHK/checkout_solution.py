39a69ba81e4cb3593b0a5a4f775fa525338938e1
sbx_user1060 <sbx_user1060@ip-10-12-4-196.eu-west-2.compute.internal>
2018-06-22 16:26:06 +0000

    Fri Jun 22 16:26:06 UTC 2018

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -3,4 +3,21 @@
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
-    raise NotImplementedError()
+
+    output = 0
+    n = 1
+    if skus == "A":
+
+        output = 130
+    elif skus == "B":
+        output = 45
+
+    elif skus == "C":
+        output = 20*n
+    elif skus == "D":
+        output = 15*n
+    else:
+        return -1
+
+    return output
+
